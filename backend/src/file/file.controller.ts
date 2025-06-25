0dc36f479774ffb0e2847eeada34b679f638d132
zh-blog-ru <zhblog.ru@yandex.ru>
2025-06-25 14:40:03 +0300

    Commit

--- backend/src/file/file.controller.ts
+++ backend/src/file/file.controller.ts
@@ -4,11 +4,12 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 import { Response } from 'express';
 import { ProfileUrlDTO } from 'DTO/ProfileUrlDto.dto';
+import { isPublic } from 'Generated/PublicDecorator';
 
 @Controller('file')
 export class FileController {
 
-
+    @isPublic()
     @Get('photo/public/:profile_picture_url')
     getPhoto(@Param(ValidationPipe) {profile_picture_url}: ProfileUrlDTO, @Res() res: Response) {
 
