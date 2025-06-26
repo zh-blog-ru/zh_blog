bae77ea29779c5e7072ffebc8d31f9f28d97ce0c
zh-blog-ru <zhblog.ru@yandex.ru>
2025-06-26 16:05:27 +0300

    Commit

--- backend/src/errors/errors.controller.ts
+++ backend/src/errors/errors.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { isPublic } from 'Generated/PublicDecorator';
 
 @Controller('errors')
 export class ErrorsController {
 
+    @isPublic()
     @Post()
     getErrors(@Body() erros: any) {
         console.log('ERRORS')
