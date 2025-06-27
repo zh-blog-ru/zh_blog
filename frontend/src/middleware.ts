5d7cde8acf4876f8d232c8191c017820b78bf926
zh-blog-ru <zhblog.ru@yandex.ru>
2025-06-27 19:30:25 +0300

    Commit

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -76,6 +76,6 @@ export function middleware(request: NextRequest) {
 }
 export const config = {
   matcher: ['/',
-    '/((?!api|_next/static|_next/image|favicon.ico|.+[.](?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml|.+[.](?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
