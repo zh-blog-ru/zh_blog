1a305b4cbeaa6d69eecca13c260c12dfbee53be3
zh-blog-ru <zhblog.ru@yandex.ru>
2025-07-10 22:56:17 +0200

    Commit

--- backend/src/file/file.module.ts
+++ backend/src/file/file.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [FileController],
   providers: [FileService],
   exports: [FileService]
